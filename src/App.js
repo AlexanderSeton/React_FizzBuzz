35b054848f838fe48a4be05e26c3b2bc4edbeb2c
Craig Marshall <dev.craigmarshall@gmail.com>
2021-11-23 13:10:22 +0000

    tried to add a timer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   let [number,setNumber] = useState(1);
   let [output,setOutput] = useState("");
   let [score,setScore] = useState(0);
+  let [time, setTime] = useState(5)
  
   // const handleAdding = () => {
   //   setNumber(number + 1)
@@ -21,6 +22,34 @@ function App() {
   //   setScore(score + 1);
   // };
 
+  // useEffect(() => {
+  //   countDown();
+  // },[number])
+
+  // const timeReduce = () => {
+  //   setTime(time - 1);
+  //   console.log("TIME:", time);
+  //   if (time == 0){
+  //     handleReset();
+  //   }
+  // }
+
+
+
+  // const countDown = () => {
+  //   for (let i = 0; i < 5; i++){
+  //     wait(1000).
+  //     setTime(time - 1);
+  //     timerCheck();
+  //   }
+  // }
+
+  // const timerCheck = () => {
+  //   if (time == 0) {
+  //     handleReset();
+  //   }
+  // }
+
   const calculateScore = (event) => {
     const answer = event.target.value;
     if (output == answer){
@@ -35,10 +64,17 @@ function App() {
   const handleReset = () => {
     setScore(0);
     setNumber(1);
+    setTime(5);
   }
 
   useEffect(() => {
     console.log("Trigger")
+    setInterval(function(){
+      setTime(time - 1);
+      console.log("TIME:", time);
+      if (time == 0){
+        handleReset();
+      }}, 1000);
     if (number % 3 === 0 && number % 5 === 0){
       setOutput("FizzBuzz");
     } else if (number % 3 === 0) {
@@ -48,14 +84,14 @@ function App() {
     } else {
       setOutput(number);
     }
-  },[number])
+  },[number]);
 
   return (
     <div>
       {/* <h1>Output: {output}</h1> */}
       {/* <button onClick={handleAdding}>+</button>
       <button onClick={handleMinus}>-</button> */}
-      
+      <p>Countdown..{time}</p>
       <p>{number}</p>
       {/* <button onClick={handleAddingScore}>Correct</button> */}
       <button value="Fizz" onClick={calculateScore}>Fizz</button>
@@ -66,7 +102,7 @@ function App() {
       <br />
       <button onClick={handleReset}>Reset</button>
 
-      <p>ScoreTally: {score}</p>
+      <p>Score Tally: {score}</p>
     </div>
   );
 }
