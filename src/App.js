b3758522992f0a8ac21fdf53102998db3bdbfbd6
Craig Marshall <dev.craigmarshall@gmail.com>
2021-11-23 11:40:54 +0000

    Extension completed

--- src/App.js
+++ src/App.js
@@ -5,35 +5,51 @@ function App() {
 
   let [number,setNumber] = useState(1);
   let [output,setOutput] = useState("");
+  let [score,setScore] = useState(0);
  
   const handleAdding = () => {
     setNumber(number + 1)
   }
 
-  const determineFizzBuzz = () => {
-    if (number % 3 == 0 && number % 5 == 0){
-      setOutput("Fizzbuzz")
+  const handleMinus = () => {
+    if (number > 1){
+      setNumber(number - 1)
     }
   }
 
+  const handleAddingScore = () => {
+    setScore(score + 1);
+  };
+
+  const handleReset = () => {
+    setScore(0);
+    setNumber(1);
+  }
+
   useEffect(() => {
-    if (number % 3 == 0 && number % 5 == 0){
+    console.log("Trigger")
+    if (number % 3 === 0 && number % 5 === 0){
       setOutput("Fizzbuzz");
-    } else if (number % 3 == 0) {
+    } else if (number % 3 === 0) {
       setOutput("Fizz");
-    } else if (number % 5 == 0) {
+    } else if (number % 5 === 0) {
       setOutput("Buzz");
     } else {
       setOutput(number);
     }
-  })
+  },[number])
 
   return (
     <div>
       <h1>Output: {output}</h1>
       <button onClick={handleAdding}>+</button>
-      {/* <button onClick={handleMinus}>-</button> */}
+      <button onClick={handleMinus}>-</button>
+      
       <p>{number}</p>
+      <button onClick={handleAddingScore}>Correct</button>
+      <button onClick={handleReset}>Reset</button>
+
+      <p>ScoreTally: {score}</p>
     </div>
   );
 }
